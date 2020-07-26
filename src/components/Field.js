0e04708c0ba087ea7b50e7d98dd6abadfcada813
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-07-26 20:41:53 -0300

    Criar component Mine

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,28 +1,39 @@
 import React from 'react';
-
-import params from '../params';
 import {View, StyleSheet, Text} from 'react-native';
 
-export default ({mined, opened, nearMines}) => {
+import params from '../params';
+import Mine from './Mine';
+
+export default ({mined, opened, nearMines, exploded}) => {
   const styleField = [styles.field];
+
   if (opened) {
     styleField.push(styles.opened);
   }
+
+  if (exploded) {
+    styleField.push(styles.exploded);
+  }
+
   if (styleField.length === 1) {
     styleField.push(styles.regular);
   }
 
   let color = null;
+
   if (nearMines > 0) {
     if (nearMines == 1) {
       color = '#2a28d7';
     }
+
     if (nearMines == 2) {
       color = '#2b520f';
     }
+
     if (nearMines > 2 && nearMines < 6) {
       color = '#f9060a';
     }
+
     if (nearMines >= 6) {
       color = '#f221a9';
     }
@@ -35,6 +46,7 @@ export default ({mined, opened, nearMines}) => {
       ) : (
         false
       )}
+      {mined && opened ? <Mine /> : false}
     </View>
   );
 };
@@ -61,5 +73,9 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: 'bold',
     fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
   },
 });
