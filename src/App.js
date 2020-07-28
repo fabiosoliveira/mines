010f5d6b4d076edf824c3bcaf61eb1d5edd34dee
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-07-28 20:21:27 -0300

    Criar lógica abrir campo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Alert} from 'react-native';
 
 import params from './params';
 import MineField from './components/MineField';
-import {createMinedBoard} from './logics';
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  showMines,
+  wonGame,
+  hadExplosion,
+} from './logics';
 
 function minesAmount() {
   const cols = params.getColumnsAmount();
@@ -18,7 +25,29 @@ function createState() {
 }
 
 export default (props) => {
-  const board = createState();
+  const [board, setBoard] = useState(createState());
+  const [won, setWon] = useState(false);
+  const [lost, setLost] = useState(false);
+
+  function onOpenField(row, column) {
+    const _board = cloneBoard(board);
+    openField(_board, row, column);
+    const _lost = hadExplosion(_board);
+    const _won = wonGame(_board);
+
+    if (_lost) {
+      showMines(_board);
+      Alert.alert('Perdeeeeu!', 'Que Buuuuurro!');
+    }
+
+    if (_won) {
+      Alert.alert('Parabéns', 'Você vençeu!');
+    }
+
+    setBoard(_board);
+    setWon(_won);
+    setLost(_lost);
+  }
 
   return (
     <View style={styles.container}>
@@ -29,7 +58,7 @@ export default (props) => {
       </Text>
 
       <View style={styles.board}>
-        <MineField board={board} />
+        <MineField board={board} onOpenField={onOpenField} />
       </View>
     </View>
   );
