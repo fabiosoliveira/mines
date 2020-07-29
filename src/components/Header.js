6d339d804b60979e615f71929377d1a9a0ab6950
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-07-28 21:10:23 -0300

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   flagContainer: {
-    flexDirection: 'row',,
+    flexDirection: 'row',
   },
   flagButton: {
     marginTop: 10,
